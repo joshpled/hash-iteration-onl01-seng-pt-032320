d31e4c40098c2683e5153c5ca70fc4fc4f1aa29a
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-03 06:11:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,5 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each {|kids_name, age| age <= 12 puts ? "Happy Birthday #{kids_name}! You are now #{age} years old!" : "You are too old for this."}
+  birthday_kids.each {|kids_name, age| age <= 12 ? puts "Happy Birthday #{kids_name}! You are now #{age} years old!" : puts "You are too old for this."}
 end
